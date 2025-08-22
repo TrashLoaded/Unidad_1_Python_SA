bb23c54f9c4cadb994033be7bc1468cfdc195ae1
TrashLoaded <alexiseba15@gmail.com>
2025-08-22 18:41:07 -0400

    commit de actividad 1

--- monitoreo/dispositivos/views.py
+++ monitoreo/dispositivos/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+def panel_dispositivos(request):
+    dispositivos = [
+        {"nombre":"Sensor Temperatura","consumo":50},
+        {"nombre":"Medidor Solar","consumo":120},
+        {"nombre":"Sensor Movimiento","consumo":30},
+        {"nombre":"Calefactor","consumo":200},
+    ]
+    
+    consumo_maximo = 100
+    
+    for d in dispositivos:
+        if d["consumo"] <= consumo_maximo:
+            d["estado"] = "Correcto"
+        else:
+            d["estado"] = "Exceso"
+    
+    return render(request, "dispositivos/panel.html",{
+        "dispositivos":dispositivos,
+        "consumo_maximo":consumo_maximo
+    })
--- monitoreo/monitoreo/urls.py
+++ monitoreo/monitoreo/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from dispositivos.views import panel_dispositivos
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',panel_dispositivos),
 ]
