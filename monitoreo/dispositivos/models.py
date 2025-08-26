eb40ba7970d652c560298aa10d0a3bd670da1c8b
Laboratorio Computación N° 8 <claseslab8@inacap.cl>
2025-08-26 18:08:01 -0400

    commit 26-08

--- monitoreo/dispositivos/models.py
+++ monitoreo/dispositivos/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
+
+class Dispositivos(models.Model):
+    nombre = models.CharField(max_length=100)
+    consumo_maximo = models.IntegerField()
+    estado = models.BooleanField(default=True)
+    categoria = models.ForeignKey(Categoria,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
